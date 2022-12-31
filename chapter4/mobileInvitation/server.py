5f1bf392f7638eb3d87db02a7483d818d115da4d
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-12-31 17:36:18 +0900

    feat: [모바일 청첩장] 방명록 목록 pagination 기능 구현

--- chapter4/mobileInvitation/server.py
+++ chapter4/mobileInvitation/server.py
@@ -1,6 +1,7 @@
 import datetime
 from flask import Flask, render_template, request, redirect
 from flask_pymongo import PyMongo
+import math
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/local'
@@ -24,10 +25,24 @@ def index():
     wedding = datetime.datetime(2023, 2, 18, 0, 0, 0) # 결혼식 날짜
     diff = (wedding - now).days
 
-    guests = mongo.db.wedding.find()
-    
-    
-    return render_template('index.html', diff=diff, guests=guests)
+    page = int(request.args.get('page', 1)) # 사용자가 요청한 주소에 'page'라는 data 있으면 가지고 오고, 없으면 1을 가져오기
+    limit = 3
+    skip = (page - 1) * limit
+
+    # skip값만큼 스킵한 이후 limit값만 가지고 오기
+    guests = mongo.db.wedding.find().limit(limit).skip(skip)
+    # count_documents : 조건에 일치하는 documents 개수 가지고 오기
+    countOfAllGuests = mongo.db.wedding.estimated_document_count()
+    maxPage = math.ceil(countOfAllGuests / limit)
+
+    pages = list(range(1, maxPage + 1))
+
+    return render_template(
+        'index.html', 
+        diff=diff, 
+        guests=guests, 
+        pages=pages,
+        countOfAllGuests = countOfAllGuests)
 
 if __name__ == '__main__':
     app.run()
